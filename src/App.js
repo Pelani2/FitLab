45441ec1d7344996a2352f2478115ebfe7b295d3
Igor Zupunski <ipatheword@live.com>
2023-07-05 22:24:00 +0200

    space

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.js";
 
+
+
 export default function App() {
   return(
     <RouterProvider router={AppRoutes} />
