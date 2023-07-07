4f4a1eddc7c4ecd0f697b0f9cc5da62270a7f028
Igor Zupunski <ipatheword@live.com>
2023-07-07 12:51:11 +0200

    applied basic elements for jobs page

--- src/pages/jobsPage/JobsPage.js
+++ src/pages/jobsPage/JobsPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/JobsStyles.css";
 import ApplyNowButton from "../../components/buttons/applyNowButton/ApplyNowButton";
+import { Link } from "react-router-dom";
 
 export default function JobsPage() {
     return(
@@ -10,12 +11,44 @@ export default function JobsPage() {
             </h1>
             <div className="job-item" >
                 <h3>
-                    Job Position
+                    Personal Trainer
                 </h3>
                 <p>
-                    Ullamco incididunt anim proident minim in occaecat consequat in sit duis cupidatat.Eiusmod qui velit cupidatat do dolore eu incididunt.
+                    We are looking for a certified personal trainer to join our team at
+                    Island FitLab. The ideal candidate should have experience in guiding
+                    and motivating clients to achieve their fitness goals.
                 </p>
-                <ApplyNowButton text="Apply Now" />
+                <Link>
+                    <ApplyNowButton text="Apply Now" />
+                </Link>
+            </div>
+
+            <div className="job-item" >
+                <h3>
+                    Group Fitness Instructor
+                </h3>
+                <p>
+                    Island FitLab is seeking a dynamic and energetic group fitness
+                    instructor. Experience in leading various group fitness classes such
+                    as Zumba, Yoga, or HIIT is preferred.
+                </p>
+                <Link>
+                    <ApplyNowButton text="Apply Now" />
+                </Link>
+            </div>
+
+            <div className="job-item" >
+                <h3>
+                    Front Desk Receptionist
+                </h3>
+                <p>
+                    We are hiring a friendly and organized front desk receptionist to
+                    provide exceptional customer service at Island FitLab. Responsibilities
+                    include greeting members, scheduling appointments, and handling inquiries.
+                </p>
+                <Link>
+                    <ApplyNowButton text="Apply Now" />
+                </Link>
             </div>
         </div>
     );
