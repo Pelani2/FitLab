2e3418c5df99a5a845afa417e98b94e7a512ee1d
Igor Zupunski <ipatheword@live.com>
2023-07-08 11:40:34 +0200

    added form elements

--- src/pages/applyNowPage/ApplyNowPage.js
+++ src/pages/applyNowPage/ApplyNowPage.js
@@ -34,7 +34,79 @@ export default function ApplyNowPage() {
 
     return(
         <div>
+            <h1>
+                Job Application
+            </h1>
+            {message && <p>{message}</p>}
+            <form onSubmit={handleSubmit} >
+                <div>
+                    <label htmlFor="name" >
+                        Name: 
+                    </label>
+                    <input 
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={handleNameChange}
+                        required
+                    />
+                </div>
 
+                <div>
+                    <label htmlFor="email" >
+                        Email: 
+                    </label>
+                    <input 
+                        type="text"
+                        id="name"
+                        value={email}
+                        onChange={handleEmailChange}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="phone" >
+                        Phone: 
+                    </label>
+                    <input 
+                        type="text"
+                        id="name"
+                        value={phone}
+                        onChange={handlePhoneChange}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="resume" >
+                        Resume:
+                    </label>
+                    <input 
+                        type="file"
+                        id="resume"
+                        value={resume}
+                        onChange={handleResumeChange}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="coverLetter" >
+                        Cover Letter:
+                    </label>
+                    <input 
+                        type="file"
+                        id="coverLetter"
+                        value={coverLetter}
+                        onChange={handleCoverLetterChange}
+                        required
+                    />
+                </div>
+                <button type="submit" >
+                    Submit Application
+                </button>
+            </form>
         </div>
     );
 }
