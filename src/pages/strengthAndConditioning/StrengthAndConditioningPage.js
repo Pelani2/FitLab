49a0e0adb03844f1492f066acbad0e5622417146
Igor Zupunski <ipatheword@live.com>
2023-07-10 12:48:18 +0200

    added more elements

--- src/pages/strengthAndConditioning/StrengthAndConditioningPage.js
+++ src/pages/strengthAndConditioning/StrengthAndConditioningPage.js
@@ -4,26 +4,83 @@ import "../../styles/StrengthAndConditioningPage.css";
 export default function StrengthAndConditioningPage() {
     return(
         <div>
-            <h2>
-                Exercises
-            </h2>
-            <ul>
-                <li>
-                    Compound Movements
-                </li>
-                <li>
-                    Olympic Lifts
-                </li>
-                <li>
-                    Plyometric Exercises
-                </li>
-                <li>
-                    Interval Training
-                </li>
-                <li>
-                    Agility Drills
-                </li>
-            </ul>
+            <section>
+                <h2>
+                    Most popular exercises
+                </h2>
+                <ul>
+                    <li>
+                        Compound Movements
+                    </li>
+                    <li>
+                        Olympic Lifts
+                    </li>
+                    <li>
+                        Plyometric Exercises
+                    </li>
+                    <li>
+                        Interval Training
+                    </li>
+                    <li>
+                        Agility Drills
+                    </li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>
+                    Benefits
+                </h2>
+                <p>
+                    Strength and conditioning training offers numerous benefits for individuals seeking to improve their fitnesss and overall performance. Some of the key benefits include:
+                </p>
+                <ul>
+                    <li>
+                        Increased strength and power
+                    </li>
+                    <li>
+                        Improved endurance and stamina
+                    </li>
+                    <li>
+                        Enhanced athletic performance
+                    </li>
+                    <li>
+                        Better body composition and muscle tone
+                    </li>
+                    <li>
+                        Injury prevention and imroved joint stability
+                    </li>
+                    <li>
+                        Increased bone density and reduced risk of osteroporosis
+                    </li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>
+                    Best cases for choose this trainig
+                </h2>
+                <p>
+                    Strength and conditioning training is particularly beneficial in the following cases:
+                </p>
+                <ul>
+                    <li>
+                        Athletes aiming to enhance their sports performance.
+                    </li>
+                    <li>
+                        Individuals looking to improve overall fitness and functional strength.
+                    </li>
+                    <li>
+                        People seeking weight managment and body composition goals.
+                    </li>
+                    <li>
+                        Individuals recovering from injury or surgery, under professional guidance.
+                    </li>
+                    <li>
+                        Older adults aiming to maintain muscle mass, bone health, and functional independance.
+                    </li>
+                </ul>
+            </section>
         </div>
     );
 }
