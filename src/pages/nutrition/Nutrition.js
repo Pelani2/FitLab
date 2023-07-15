c026369b0011a4d9b6b5e32ec32b65046a3f49eb
Igor Zupunski <ipatheword@live.com>
2023-07-15 12:20:03 +0200

    applied some styling

--- src/pages/nutrition/Nutrition.js
+++ src/pages/nutrition/Nutrition.js
@@ -29,85 +29,88 @@ export default function Nutrition() {
     ];
 
     return(
-        <div className="nutrition">
-            <h1 className="nutrition__title">
-                Nutrition Counseling
-            </h1>
-            <p className="nutrition__desc">
-                Welcome to the Nutrition Counseling page! Here you'll find information
-                and guidance on achieving a balanced and healthy diet through
-                personalized counseling sessions.
-            </p>
-  
-            <h2 className="nutrition__title">
-                What is Nutrition Counseling?
-            </h2>
-            <p className="nutrition__desc">
-                Nutrition counseling is a process where a qualified nutritionist or
-                dietitian provides personalized guidance and support to help individuals
-                make informed decisions about their diet and lifestyle. It involves
-                assessing dietary needs, setting goals, and developing strategies to
-                achieve optimal health and well-being.
-            </p>
-  
-        <h2 className="nutrition__title">
-            Benefits of Nutrition Counseling
-        </h2>
-        <ul className="nutirition__list">
-            {benefitsOfNutrition.map(benefit => (
-                <li 
-                    key={benefit}
-                    className="nutrition__list-item"
-                >
-                    {benefit}
-                </li>
-            ))}
-        </ul>
-  
-        <h2 className="nutrition__title">
-            What to Expect in a Counseling Session
-        </h2>
-        <p className="nutrition__desc">
-          During a nutrition counseling session, you can expect the following:
-        </p>
-        <ul className="nutirition__list">
-          {expectations.map(expectation => (
-            <li 
-                key={expectation} 
-                className="nutrition__list-item"
-            >
-                {expectation}
-            </li>
-          ))}
-        </ul>
-  
-        <h2 className="nutrition__title">
-            Is Nutrition Counseling Right for You?
-        </h2>
-        <p className="nutrition__desc">
-          Nutrition counseling can benefit anyone looking to improve their diet, manage specific health conditions, or optimize their athletic performance. Whether you want to lose weight, manage diabetes, improve digestion, or simply adopt healthier eating habits, a nutritionist or dietitian can provide expert guidance and support tailored to your
-          unique needs.
-        </p>
+        <div className="nutrition__background"> 
+            <div className="nutrition">
+                <h1 className="nutrition__title">
+                    Nutrition Counseling
+                </h1>
+                <p className="nutrition__desc">
+                    Welcome to the Nutrition Counseling page! Here you'll find information
+                    and guidance on achieving a balanced and healthy diet through
+                    personalized counseling sessions.
+                </p>
+    
+                <h2 className="nutrition__title">
+                    What is Nutrition Counseling?
+                </h2>
+                <p className="nutrition__desc">
+                    Nutrition counseling is a process where a qualified nutritionist or
+                    dietitian provides personalized guidance and support to help individuals
+                    make informed decisions about their diet and lifestyle. It involves
+                    assessing dietary needs, setting goals, and developing strategies to
+                    achieve optimal health and well-being.
+                </p>
+    
+                <h2 className="nutrition__title">
+                    Benefits of Nutrition Counseling
+                </h2>
+                <ul className="nutirition__list">
+                    {benefitsOfNutrition.map(benefit => (
+                        <li 
+                            key={benefit}
+                            className="nutrition__list-item"
+                        >
+                            {benefit}
+                        </li>
+                    ))}
+                </ul>
+    
+                <h2 className="nutrition__title">
+                    What to Expect in a Counseling Session
+                </h2>
+                <p className="nutrition__desc">
+                    During a nutrition counseling session, you can expect the following:
+                </p>
+                <ul className="nutirition__list">
+                {expectations.map(expectation => (
+                    <li 
+                        key={expectation} 
+                        className="nutrition__list-item"
+                    >
+                        {expectation}
+                    </li>
+                ))}
+                </ul>
+    
+                <h2 className="nutrition__title">
+                    Is Nutrition Counseling Right for You?
+                </h2>
+                <p className="nutrition__desc">
+                    Nutrition counseling can benefit anyone looking to improve their diet, manage specific health conditions, or optimize their athletic performance. Whether you want to lose weight, manage diabetes, improve digestion, or simply adopt healthier eating habits, a nutritionist or dietitian can provide expert guidance and support tailored to your
+                    unique needs.
+                </p>
 
-        <h2 className="nutrition__title">
-            Our top nutritionists
-        </h2>
-        <p className="nutrition__desc">
-            If you're looking for exceptional nutrition counseling services, consider consulting with one of these highly recommended nutritionists at Island Fitlab:
-        </p>
-        <ul className="nutirition__list">
-            {nutritionists.map(nutritionist => (
-                <li 
-                    key={nutritionist} 
-                    className="nutrition__list-item"
-                >
-                    {nutritionist}
-                </li>
-            ))}
-        </ul>
-        <p className="nutrition__desc">
-            These nutritionists have a wealth of experience and expertise in providing personalized nutrition counseling to help you reach your health and wellness goals.
-        </p>
-      </div>
+                <h2 className="nutrition__title">
+                    Our top nutritionists
+                </h2>
+                <p className="nutrition__desc">
+                    If you're looking for exceptional nutrition counseling services, consider consulting with one of these highly recommended nutritionists at Island Fitlab:
+                </p>
+                <ul className="nutirition__list">
+                    {nutritionists.map(nutritionist => (
+                        <li 
+                            key={nutritionist} 
+                            className="nutrition__list-item"
+                        >
+                            {nutritionist}
+                        </li>
+                    ))}
+                </ul>
+                <p className="nutrition__desc">
+                    These nutritionists have a wealth of experience and expertise in providing personalized nutrition counseling to help you reach your health and wellness goals.
+                </p>
+            </div>
+        </div>
+
     );
 }
